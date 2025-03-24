58696dc9d3ae27937930775a34ef5dbdc3314ce8
creategamesalone <creategamesalone@gmail.com>
2025-03-25 02:06:50 +0700

    25/03/2025 : update db

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
 CREATE TABLE `users` (
   `id` INT PRIMARY KEY AUTO_INCREMENT,
-  `username` VARCHAR(100) UNIQUE NOT NULL,
+  `fullname` VARCHAR(100) UNIQUE NOT NULL,
   `email` VARCHAR(150) UNIQUE NOT NULL,
-  `password_hash` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
   `role_id` INT NOT NULL,
   `created_by` INT,
   `updated_by` INT,
@@ -131,6 +131,20 @@ CREATE TABLE `audit_logs` (
   `old_value` VARCHAR(255),
   `new_value` VARCHAR(255),
   `timestamp` TIMESTAMP
+);
+
+CREATE TABLE `cities` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(100) UNIQUE NOT NULL,
+  `created_at` TIMESTAMP,
+  `updated_at` TIMESTAMP
+);
+
+CREATE TABLE `districts` (
+  `id` INT PRIMARY KEY AUTO_INCREMENT,
+  `name` VARCHAR(100) UNIQUE NOT NULL,
+  `created_at` TIMESTAMP,
+  `updated_at` TIMESTAMP
 );
 
 ALTER TABLE `users` ADD FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`) ON DELETE CASCADE;
